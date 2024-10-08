4afd67ee2e7e6d512bd52cbac6c1e35c4fb76deb
Jose Parra <110458931+JoseParra28@users.noreply.github.com>
2024-10-08 19:07:45 +0100

    navbar
    added a responsive navbar

--- script/script.js
+++ script/script.js
@@ -1,1 +1,8 @@
-console.log("Hello")
+function showSideBar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = 'flex';
+}
+function hideSideBar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = 'none';
+}
