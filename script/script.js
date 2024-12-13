b4c6f01bf42062dea0c86fdf5b8d507569616b6d
Jose Parra <110458931+JoseParra28@users.noreply.github.com>
2024-12-13 11:59:45 +0000

    jquery set up

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
-function showSideBar(){
-    const sidebar = document.querySelector(".sidebar");
-    sidebar.style.display = 'flex';
-}
-function hideSideBar(){
-    const sidebar = document.querySelector(".sidebar");
-    sidebar.style.display = 'none';
-}
+// function showSideBar(){
+//     const sidebar = document.querySelector(".sidebar");
+//     sidebar.style.display = 'flex';
+// }
+// function hideSideBar(){
+//     const sidebar = document.querySelector(".sidebar");
+//     sidebar.style.display = 'none';
+// }
+
+$("h1").css("color", "red")
